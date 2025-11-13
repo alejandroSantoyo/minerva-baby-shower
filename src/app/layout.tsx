23fa748e95d5ca2d8a22e5fce796b197f6c64836
Alejandro Santoyo <ASantoy1@its.jnj.com>
2025-11-13 00:14:50 -0600

    fix: solve build issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,6 @@
 import type { Metadata } from "next";
-import { Italianno, Cormorant } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
-
-// Fuente para "Cherry on the Top" (Script/Manuscrita)
-export const scriptFont = Italianno({
-  weight: "400", // El único peso disponible para Italianno
-  subsets: ["latin"],
-  variable: "--font-script", // Una variable CSS fácil de usar
-});
-
-// Fuente para "SHE IS THE" y detalles (Serif Limpia)
-export const serifFont = Cormorant({
-  weight: ["300", "400", "500"], // Importa varios pesos para flexibilidad
-  subsets: ["latin"],
-  variable: "--font-serif", // Una variable CSS fácil de usar
-});
 
 export const metadata: Metadata = {
   title: "Minerva's Baby Shower",
@@ -29,7 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${scriptFont.className} ${serifFont.className}`}>
+      <body
+      // className={`${scriptFont.className} ${serifFont.className}`}
+      >
         <Providers>{children}</Providers>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import styles from "./page.module.css";
 import "./globals.css";
 import Invitation from "@/components/Landing/Invitation";
-import Details from "@/components/Landing/Details";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <Invitation guest="Familia Santoyo" />
-      <Details passes={10} />
+      {/* <Details /> */}
     </div>
   );
 }
